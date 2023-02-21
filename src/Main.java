07da944723ff71e9e659fdade899efba92127231
KarinSakurai <karin_sakurai@sis2.saison.co.jp>
2023-02-21 11:14:45 +0900

    part1のコード修正
    ユニットテストを書きやすいようにメソッドを分割しました。
    取得結果はかわりません。

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
+        String path="src/sampleFile/weather .dat";
         Part1 p1=new Part1();
-        String ans=p1.part1Exe();
+        String ans=p1.part1Exe(path);
         System.out.println(ans);
     }
 }
--- src/Part1.java
+++ src/Part1.java
@@ -7,32 +7,20 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Part1 {
-    public String part1Exe(){
-        String path="src/sampleFile/weather .dat";
+    public String part1Exe(String path){
         Integer minSpreadDay=0;
 
         try(BufferedReader weatherFile= Files.newBufferedReader(Paths.get(path));){
-            Map<Integer,Integer> tempSpreadMap=new HashMap<>();
             String data;
+            Map<Integer,Integer> tempSpreadMap=new HashMap<>();
             while((data=weatherFile.readLine())!=null){
                 if(data.isEmpty()==true){
                     data="空行です";
                 }
-                String[] splitData=data.split(" ");
-                List<String> splitDataList=new ArrayList<String>(Arrays.asList(splitData));
 
-                splitDataList.removeIf(object -> object.isEmpty());
-                boolean isNumeric= splitDataList.get(0).chars().allMatch(Character::isDigit);
-                if(isNumeric==true){
-                    String strMax=splitDataList.get(1).replace("*","");
-                    String strMin=splitDataList.get(2).replace("*","");
+                List<String> splitDataList=splitDataToList(data);
+                tempSpreadMap=calcDataToMap(splitDataList,tempSpreadMap);
 
-                    int day=Integer.parseInt(splitDataList.get(0));
-                    int max=Integer.parseInt(strMax);
-                    int min=Integer.parseInt(strMin);
-                    int spread=max-min;
-                    tempSpreadMap.put(day,spread);
-                }
             }
             minSpreadDay=Collections.min(tempSpreadMap.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
             String ans="温度差の最も小さい日付は"+String.valueOf(minSpreadDay)+"日です。";
@@ -41,5 +29,28 @@ public class Part1 {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public List<String> splitDataToList(String data){
+        String[] splitData=data.split(" ");
+        List<String> splitDataList=new ArrayList<String>(Arrays.asList(splitData));
+
+        splitDataList.removeIf(object -> object.isEmpty());
+        return splitDataList;
+    }
+
+    public Map<Integer,Integer> calcDataToMap(List<String> splitDataList,Map<Integer,Integer> tempSpreadMap){
+        boolean isNumeric= splitDataList.get(0).chars().allMatch(Character::isDigit);
+        if(isNumeric==true){
+            String strMax=splitDataList.get(1).replace("*","");
+            String strMin=splitDataList.get(2).replace("*","");
+
+            int day=Integer.parseInt(splitDataList.get(0));
+            int max=Integer.parseInt(strMax);
+            int min=Integer.parseInt(strMin);
+            int spread=max-min;
+            tempSpreadMap.put(day,spread);
+        }
+        return tempSpreadMap;
     }
 }
