6d3def94b543a5433ca262a04503f0fc9470a90f
KarinSakurai <karin_sakurai@sis2.saison.co.jp>
2023-02-24 13:58:54 +0900

    part1の答えの出し方修正
    part1に対する答えを「日付　最大値　最小値」という
    形式で出力されるように変更いたしました

--- src/Part1.java
+++ src/Part1.java
@@ -1,17 +1,23 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Part1 extends AbstractClass {
+    List<Map<String, Object>> mapInList = new ArrayList<>();
+
     public String fileReadExe(String path) throws IOException {
 
         try (BufferedReader weatherFile = Files.newBufferedReader(Paths.get(path));) {
             String data;
             Map<String, Integer> tempSpreadMap = new HashMap<>();
+
             while ((data = weatherFile.readLine()) != null) {
-                if (data.isEmpty() == true) {
+                if (data.isEmpty()) {
                     data = "空行です";
                 }
                 List<String> splitDataList = splitDataToList(data);
@@ -19,7 +25,12 @@ public class Part1 extends AbstractClass {
 
             }
             String minSpreadDay = minDataSelect(tempSpreadMap);
-            String ans = "温度差の最も小さい日付は" + minSpreadDay + "日です。";
+
+            Map<String, Object> answer = mapInList.stream()
+                    .filter(a -> (minSpreadDay.equals(a.get("day"))))
+                    .findAny().get();
+
+            String ans = answer.get("day") + " " + answer.get("max") + " " + answer.get("min");
             return ans;
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,6 +41,7 @@ public class Part1 extends AbstractClass {
     @Override
     public Map<String, Integer> calcDataToMap(List<String> splitDataList, Map<String, Integer> tempSpreadMap) {
         boolean isNumeric = splitDataList.get(0).chars().allMatch(Character::isDigit);
+        Map<String, Object> map = new HashMap<>();
         if (isNumeric) {
             String strMax = splitDataList.get(1).replace("*", "");
             String strMin = splitDataList.get(2).replace("*", "");
@@ -38,7 +50,13 @@ public class Part1 extends AbstractClass {
             int min = Integer.parseInt(strMin);
             int spread = max - min;
             tempSpreadMap.put(splitDataList.get(0), spread);
+
+            map.put("day", splitDataList.get(0));
+            map.put("max", max);
+            map.put("min", min);
+            mapInList.add(map);
         }
         return tempSpreadMap;
     }
+
 }
